08cae84063de6b8f1c6c3f046464fd2e58736046
Dhanushka Ganhewage <50699988+DhanuSG@users.noreply.github.com>
2020-04-15 16:28:13 +0530

    my first commit

---
+++ HelthCare/src/com/paf/patient/dao/Doctor.java
@@ -0,0 +1,22 @@
+package com.paf.patient.dao;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class Doctor {
+	
+		public Connection getConnection( ) {
+
+			Connection con = null;
+
+			try {
+				Class.forName("com.mysql.jdbc.Driver");
+				con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HelthCare_doctor", "root", "root");
+			} catch(Exception e) {
+				e.printStackTrace();
+			}
+
+			return con;
+		}
+
+	}
+
