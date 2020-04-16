b0da993f4e0897fa6c5b286468ac76905378182d
prabhashwara <lankeshprabhashwara@gmail.com>
2020-04-16 17:56:52 +0530

    added update option

--- HelthCare/src/com/paf/patient/model/PatientDAO.java
+++ HelthCare/src/com/paf/patient/model/PatientDAO.java
@@ -54,5 +54,35 @@ public class PatientDAO {
 		return output;
 		
 	}
+	
+	public static String updatePatient(Patient patient) {
+		String status = null;
+		
+		try {
+			Connection con = getConnection();
+			
+			String queary = "update patient_registration set firstName=?, lastName=?, dob=?, gender=?"
+					+ "email=?, patientPassword=? where nic=?";
+			
+			PreparedStatement ps = con.prepareStatement(queary);
+			
+			ps.setString(1, patient.getFirstName());
+			ps.setString(2, patient.getLastName());
+			ps.setString(3, patient.getDob());
+			ps.setString(4, patient.getGender());
+			ps.setString(5, patient.getEmail());
+			ps.setString(6, patient.getPassword());
+			ps.setString(7, patient.getNic());
+			
+			ps.executeUpdate();
+			
+			status = "Patient Update Successfully";
+		} catch (Exception e) {
+			e.printStackTrace();
+			status = "Error in update process";
+		}
+		
+		return status;
+	}
 
 }
--- HelthCare/src/com/paf/patient/service/PatientService.java
+++ HelthCare/src/com/paf/patient/service/PatientService.java
@@ -32,6 +32,27 @@ public class PatientService {
 		return status;
 	}
 	
+	@PUT
+	@Path("patient_update")
+	@Consumes(MediaType.APPLICATION_JSON)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String updatePatient(String patientData) {
+		JsonObject patient = new JsonParser().parse(patientData).getAsJsonObject();
+		
+		String nic = patient.get("nic").getAsString();
+		String firstName = patient.get("firstName").getAsString();
+		String lastName = patient.get("lastName").getAsString();
+		String dob = patient.get("dob").getAsString();
+		String gender = patient.get("gender").getAsString();
+		String email = patient.get("email").getAsString();
+		String password = patient.get("password").getAsString();
+		
+		Patient p = new Patient(nic, firstName, lastName, dob, gender, email, password);
+		
+		String output = PatientDAO.updatePatient(p);
+		
+		return output;
+	}
 	
 	
 }
