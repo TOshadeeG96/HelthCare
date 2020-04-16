fcb5e872d60a239137b3ba7b7be426658585b9f4
TOshadeeG96 <oshadeekaps@gmail.com>
2020-04-16 18:53:38 +0530

    added a jsp file

--- HelthCare/src/com/paf/appoinment/model/Appoinments.java
+++ HelthCare/src/com/paf/appoinment/model/Appoinments.java
@@ -1,7 +1,24 @@
 package com.paf.appoinment.model;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+
 public class Appoinments {
 	
+public static Connection getConnection( ) {
+		
+		Connection con = null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HealthCare_Appoinment", "root", "root123");
+			System.out.println("Successfully Connected!");
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+		return con;
+	}
 	
 	
 
--- HelthCare/src/com/paf/patient/model/PatientDAO.java
+++ HelthCare/src/com/paf/patient/model/PatientDAO.java
@@ -15,9 +15,11 @@ public class PatientDAO {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			con = DriverManager.getConnection("jdbc:mysql://localhost:3306/HelthCare_patient", "root", "admin123");
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
+		
 		
 		return con;
 	}
