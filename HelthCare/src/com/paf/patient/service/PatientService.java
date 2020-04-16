1ba22a139e87a16bbe81afb6c18d421632b2f8b0
prabhashwara <lankeshprabhashwara@gmail.com>
2020-04-16 09:00:24 +0530

    added patient-register-api method

--- HelthCare/src/com/paf/patient/service/PatientService.java
+++ HelthCare/src/com/paf/patient/service/PatientService.java
@@ -1,5 +1,35 @@
 package com.paf.patient.service;
 
+import com.paf.patient.bean.Patient;
+import com.paf.patient.model.PatientDAO;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+
+import com.google.gson.*;
+
+@Path("/patient")
 public class PatientService {
 
+	@POST
+	@Path("/")
+	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Produces(MediaType.TEXT_PLAIN)
+	public String registerPatient(@FormParam("nic") String nic,
+			@FormParam("firstName") String firstName,
+			@FormParam("lastName") String lastName,
+			@FormParam("dob") String dob,
+			@FormParam("gender") String gender,
+			@FormParam("email") String email,
+			@FormParam("password") String password) {
+		
+		String status;
+		
+		Patient p = new Patient(nic, firstName, lastName, dob, gender, email, password);
+		
+		status = PatientDAO.registerPatient(p);
+		
+		return status;
+	}
+	
 }
